0b8bb5c195dc25350266b8d23ec34369c41cf13c
Tsubasa Ichida <tsubachin1984@gmail.com>
2021-08-09 21:39:09 +0900

    second commit

---
+++ docs/js/scrollTop.js
@@ -0,0 +1,17 @@
+jQuery(function($) {
+    var topBtn = $('#pageTop');
+    topBtn.hide();
+    $(window).scroll(function () {
+     if ($(this).scrollTop() > 100) {
+      topBtn.fadeIn();
+     } else {
+      topBtn.fadeOut();
+     }
+    });
+    topBtn.click(function () {
+     $('body,html').animate({
+      scrollTop: 0
+     }, 700);
+     return false;
+    });
+});
