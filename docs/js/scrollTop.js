ef50ae45cf151d7b95f38c459532c02a65c4374a
Tsubasa Ichida <tsubachin1984@gmail.com>
2021-08-11 21:19:14 +0900

    website update

--- docs/js/scrollTop.js
+++ docs/js/scrollTop.js
@@ -2,11 +2,11 @@ jQuery(function($) {
     var topBtn = $('#pageTop');
     topBtn.hide();
     $(window).scroll(function () {
-     if ($(this).scrollTop() > 100) {
-      topBtn.fadeIn();
-     } else {
-      topBtn.fadeOut();
-     }
+        if ($(this).scrollTop() > 100) {
+            topBtn.fadeIn();
+        } else {
+            topBtn.fadeOut();
+        }
     });
     topBtn.click(function () {
      $('body,html').animate({
