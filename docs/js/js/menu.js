14e837f678d6c1d590a7804edb73ade77fd6174a
Tsubasa Ichida <tsubachin1984@gmail.com>
2021-08-11 16:06:13 +0900

    4 commit

--- docs/js/js/menu.js
+++
@@ -1,7 +0,0 @@
-jQuery(function($){
-  $('.menu-btn').on('click', function(){
-    $('.menu').toggleClass('is-active');
-    $('.fa-bars').toggleClass('none');
-    $('.fa-times').toggleClass('unnone');
-  });
-});
